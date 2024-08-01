09a633c7566cedd78d76cebb46fa56f5c2c813ad
RobertoPatino1 <rapatino@espol.edu.ec>
2024-07-31 22:30:16 -0500

    Deleted comments

--- features/steps/listAllTasks.py
+++ features/steps/listAllTasks.py
@@ -10,20 +10,17 @@ to_do_list = {}
 def list_step1_impl(context):
     global to_do_list
     to_do_list = {
-        "Buy groceries": False,  # False significa que no está completada
-        "Pay bills": False       # Puedes ajustar según tu lógica
+        "Buy groceries": False,  
+        "Pay bills": False       
     }
 
 @when('the user lists all tasks')
 def list_step2_impl(context):
-    # Redirigir stdout para capturar la salida
     context.stdout = StringIO()
     sys.stdout = context.stdout
 
-    # Llamar a la función que lista las tareas
     list_all_tasks(to_do_list)
 
-    # Restaurar stdout
     sys.stdout = sys.__stdout__
 
 @then('the output should contain')
