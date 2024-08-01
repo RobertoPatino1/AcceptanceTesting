d8fbddbb9d7a7df291815dc25dd1529fd4d323fe
RobertoPatino1 <rapatino@espol.edu.ec>
2024-07-31 20:27:37 -0500

    Added requirements and aditional routines

--- main.py
+++ main.py
@@ -29,15 +29,29 @@ def mark_task_as_completed(task:str,todo_list:dict)->bool:
         return True
     print("The given task was not in the todo list")
     return False
-    
+
+
 def clear_todo_list(todo_list:dict)->None:
     print("Clearing up the todo list...")
     todo_list.clear()
 
 
+def remove_completed_tasks(todo_list:dict)->None:
+    print("Removing all the completed tasks from the list...")
+    for task,status in todo_list.items():
+        if status:
+            todo_list.pop(task)
+
+
+def change_back_to_pending(task:str, todo_list:dict)->bool:
+    if task.capitalize() in todo_list.keys():
+        todo_list[task.capitalize()] = False
+        print("Succesfully changed the task back to Pending")
+        return True
+    print("The given task was not in the todo list")
+    return False
 
 
 todo_list = {}
 add_task("Buy Groceries",todo_list)
-
 list_all_tasks(todo_list)
